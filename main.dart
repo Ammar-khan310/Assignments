f6b234a5bfae2f60e334977bc8fbf5bbe6639e77
Ammar-khan310 <ammarkhn097@gmail.com>
2025-04-18 20:22:11 +0500

    Assignment 1

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main() {
+
+print("Assalam o alaikum, Myname is Anmar Khan, and I am currently an undergraduate student at virtual Diversity &ny degree program ta software engineering."
+
+String stdname = Ammar Khan;
+
+print(stdname):
+
+Int age = 18;
+
+print(age);
+
+var university = "Virtual University":
+
+print (university):
+
+String inter = "Intermediate result"
+
+print (inter);
+
+double percentage = 52.45;
+
+print(percentage);
+}
